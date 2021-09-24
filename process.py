e993b34cf7cd642cf763ca8415315a5403a9f764
OJ42 <88906115+OJ42@users.noreply.github.com>
2021-09-24 11:10:26 -0600

    Final versions of process and sql skills

--- process.py
+++ process.py
@@ -17,13 +17,14 @@ def sales_reports(log_file):
 #call the function
 sales_reports(log_file)
 
-log_file = open("um-server-01.txt")
-def over_ten(log_file):
-    for line in log_file:  
-        line = line.rstrip()
-        inputs = line.split(':')
-        inputs2 = int(line[16:18])
-        if inputs2 > 10:
-            print(line)
+log_file = open("um-server-01.txt") # read file in
+def over_ten(log_file): #define new function
+    print("") #print a blank line to seperate from the previous function
+    for line in log_file: #loop through the opened file 
+        line = line.rstrip() #remove extra spacing from line
+        inputs = line.split(':') #split the file at the :
+        inputs2 = int(line[16:18]) #read in the characters at spot 16-18 convert to ints
+        if inputs2 > 10: #if the ints from 16-18 are greater then 10 perfrom the next line
+            print(line) #print the line if conditions are met
 
-over_ten(log_file)
+over_ten(log_file) #call the function
--- sql-skills.sql
+++ sql-skills.sql
@@ -1,7 +1,7 @@
 -- ARTISTS TABLE
 INSERT INTO artist ( name ) 
 VALUES ( 'David' ),
-			 ( 'Jessica' ),
+	( 'Jessica' ),
        ( 'Toledo' );
 SELECT * FROM artist;
 
@@ -46,3 +46,35 @@ JOIN employee e ON c.support_rep_id = e.employee_id;
 SELECT al.title, ar.name FROM album al
 JOIN artist ar ON al.artist_id = ar.artist_id;
 
+-- EXTRA CREDIT
+-- ARTISTS
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+SELECT * FROM artist WHERE name LIKE '%Black%';
+
+-- EXTRA CREDIT
+-- INVOICE
+SELECT MAX(birth_date) FROM employee;
+
+SELECT MIN(birth_date) FROM employee;
+
+-- EXTRA CREDIT
+-- JOIN QUERIES
+SELECT pt.track_id FROM playlist_track pt
+JOIN playlist p ON p.playlist_id = pt.playlist_id
+WHERE p.name = 'Music';
+
+SELECT t.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+WHERE pt.playlist_id = 5;
+
+SELECT t.name, p.name FROM track t
+JOIN playlist_track pt ON t.track_id = pt.track_id
+JOIN playlist p ON pt.playlist_id = p.playlist_id;
+
+SELECT t.name, a.title FROM track t
+JOIN album a ON t.album_id = a.album_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Alternative & Punk';
