a525b5d4467f965fbdb211d37f9971d15eb23b03
OJ42 <88906115+OJ42@users.noreply.github.com>
2021-09-24 09:08:47 -0600

    Python Comments

---
+++ process.py
@@ -0,0 +1,19 @@
+# read in the text file with our csv data
+log_file = open("um-server-01.txt")
+
+# define the function
+def sales_reports(log_file):
+    # loop through the file one line at a time
+    for line in log_file:
+        #remove the extra stuff at the end of the line
+        line = line.rstrip()
+        #take the first 3 letters of the line
+        day = line[0:3]
+        #conditional statement, if day is "Tue" then do the next indented line (changed to Mon) 
+        if day == "Mon":
+            #displays the line if the conditions are met
+            print(line)
+
+#call the function
+sales_reports(log_file)
+
